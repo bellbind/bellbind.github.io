6e1ac32498d487148ef4814f91752f647518d28f
bellbind <bellbind@gmail.com>
2013-07-11 14:48:46 +0900

    Site updated at 2013-07-11 05:48:46 UTC

---
+++ sb/script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function (ev) {
+    var ok = document.getElementById("ok");
+    ok.addEventListener("load", function (ev) {
+        alert("ok");
+    }, false);
+}, false);
